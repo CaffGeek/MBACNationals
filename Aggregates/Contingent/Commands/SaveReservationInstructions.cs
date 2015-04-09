60003c54e9c6083f9f2acb4ed4cef6fb1fc37d1e
Chad Hurd <gibble@gmail.com>
2015-04-08 20:21:28 -0500

    Fix saving redirection to root on some pages

--- Aggregates/Contingent/Commands/SaveReservationInstructions.cs
+++ Aggregates/Contingent/Commands/SaveReservationInstructions.cs
@@ -4,7 +4,7 @@ namespace MBACNationals.Contingent.Commands
 {
     public class SaveReservationInstructions
     {
-        public Guid Id;
+        public Guid Id { get; set; }
         public string Province { get; set; }
         public string Instructions { get; set; }
     }
