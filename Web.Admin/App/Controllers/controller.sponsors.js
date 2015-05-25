efecde9cc8f87729a7a2fb3aad3e5cd6655f4f98
caffgeek <gibble@gmail.com>
2015-05-24 21:25:07 -0500

    tidy up sponsors controller

--- Web.Admin/App/Controllers/controller.sponsors.js
+++ Web.Admin/App/Controllers/controller.sponsors.js
@@ -11,25 +11,6 @@
 	    vm.SaveSponsor = SaveSponsor;
 	    vm.DeleteSponsor = DeleteSponsor;
 
-	    //    "Burlington Hotel", Website: "http://www.tourismburlington.com/stay/hotels-motels/burlington-hotel-association/" },
-        //    "Holiday Inn", Website: "http://www.hiburlington.ca/" },
-        //    "C4 Centre", Website: "http://www.c4centre.com/" },
-	    //    "Meridian", Website: "http://www.meridiancu.ca/find-us/our-locations/Pages/default.aspx?lid=93" },
-        //    "Jack Astors", Website: "http://jackastors.com/location-map/4/" },
-
-        //    "Sanders", Website: "http://www.PlayWithSanders.ca" },
-        //    "Reddarc", Website: "http://www.red-d-arc.com" },
-        //    "Connectrans", Website: "http://www.connectrans.com/en/" },
-        //    "Boston Pizza", Website: "http://bostonpizza.com" },
-        //    "Tourism Hamilton", Website: "http://www.tourismhamilton.com/" },
-        //    "Hoult", Website: "http://www.houlthellewell.com/" },
-        //    "Gator Teds", Website: "http://www.gatorteds.ca/" },
-        //    "Phipps", Website: "http://www.phippsbowling.com/" },
-        //    "Greens at Renton", Website: "http://www.greensatrenton.com/" },
-        //    "Golf Depot", Website: "http://www.thegolfdepot.ca/" },
-        //    "Sysco", Website: "http://www.sysco.ca/canada/home.cfm?id=2421" },
-        //    "Elmira Bowl", Website: "http://www.elmirabowl.ca/" }
-
 	    dataService.LoadSponsors(year)
             .then(function (response) {
                 vm.Sponsors = response.data;
