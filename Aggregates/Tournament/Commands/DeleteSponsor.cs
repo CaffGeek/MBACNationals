f09e4f3578cf504d13ae1323e53810728f4334f1
caffgeek <gibble@gmail.com>
2015-05-24 20:35:20 -0500

    ability to delete sponsors

---
+++ Aggregates/Tournament/Commands/DeleteSponsor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MBACNationals.Tournament.Commands
+{
+    public class DeleteSponsor
+    {
+        public Guid Id { get; set; }
+        public string Year { get; set; }
+    }
+}
---
+++ Events/Tournament/SponsorDeleted.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Events.Tournament
+{
+    public class SponsorDeleted
+    {
+        public Guid Id;
+        public Guid SponsorId { get; set; }
+    }
+}
