3c7b97aa7d99f1b52ae13f07f7b6bd288ee2c937
caffgeek <gibble@gmail.com>
2015-06-04 20:49:53 -0500

    Starting to add Stepladder scoring, also added codeclimate config

---
+++ Web.Admin/App/Controllers/controller.stepladder.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    var stepladderController = function ($http, $location, dataService) {
+        var url = $location.absUrl();
+        var lastSlash = url.lastIndexOf('/');
+        var year = url.slice(lastSlash + 1) || new Date().getFullYear();
+
+        var vm = this;
+
+        var page = "list";
+        vm.viewUrl = '/Setup/App/Views/Stepladder/' + page + '.html';
+                
+    };
+
+    app.controller("StepladderController", ["$http", "$location", "dataService", stepladderController]);
+}());
