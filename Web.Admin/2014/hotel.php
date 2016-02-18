878cf4746829ee75c22582968aae921937819c7c
caffgeek <gibble@gmail.com>
2016-02-17 21:14:18 -0600

    Add 2014 website

---
+++ Web.Admin/2014/hotel.php
@@ -0,0 +1,93 @@
+<?php 
+include "header.php"; 
+?> 
+
+<body>
+
+<div id="skiptomain"><a href="#maincontent">skip to main content</a></div>
+
+<div id="wrapper" >
+	<div id="maincontentcontainer" >
+		<div id="maincontent">
+			
+			<div class="section group">
+				<div class="col span_3_of_3" id="header">
+				<div id="mobileLogo"><img src="images/2014_logo.png" /></div>
+				<div id="logo"><img src="images/2014_logo.png" /></div>
+				<div id="socialLinks">Join Us:&nbsp;&nbsp;<a href="https://www.facebook.com/MBAofCanada" target="_blank"><img src="images/facebook_icon.png" /></a> <a href="https://twitter.com/MBANationals"><img src="images/twitter_icon.png" /></a> <a href="http://instagram.com/mb_master_bowlers"><img src="images/instagram_icon.png" /></a></div>
+				<div id="navigation">
+          <nav class="nav-collapse" >
+            <ul>
+              <li><a href="index.php" class="first">Home</a></li>
+              <li><a href="results.php">RESULTS</a></li>
+              <li><a href="news.php">NEWS</a></li>
+              <li><a href="schedule.php">SCHEDULE</a></li>
+              <li><a href="lanedraw.php">LANE DRAW</a></li>
+              <li><a href="photos.php">PHOTOS</a></li>
+              <li><a href="contingents.php">CONTINGENTS</a></li>
+			<li><a href="http://www.tnmpromostore.com/default.aspx?business_id=2495&page=main" target="_blank">SOUVENIRS</a></li>
+              <li><a href="centres.php">CENTRES</a></li>
+              <li><a href="hotel.php">HOTEL</a></li>
+            </ul>
+          </nav>
+        </div>				
+				</div>
+			</div>
+			<div class="section group">
+				<div class="col span_3_of_3" style="margin:0px;">
+				<div id="headerImage" style="background-image:url('images/header_image_12.jpg'); "></div>
+				<div id="photoCredit"><strong>Skyline from The Forks</strong> &bull; Credit: Dan Harper</div>
+				</div>
+			</div>
+
+		<div class="section group content">
+				<div class="col span_1_of_3">
+				<div id="centres_links">
+				<img src="images/fairmont_logo.jpg" alt="fairmont winnipeg" style="width:100%;"/>
+				
+				</div>
+				</div>
+				<div class="col span_2_of_3" id="contentArea">
+					
+					<h2>The Fairmont Winnipeg</h2>
+					<p><a href="http://www.fairmont.com/winnipeg/?cmpid=google_lom_search-branded_branded-e-revsh&s_kwcid=TC|1025556|fairmont%20winnipeg||S|e|25917158460&gclid=CJzfusKA5r0CFQ5gMgodiDsAQg" target="_blank">CLICK HERE</a> to visit their website. </p>
+
+									<div id="map">
+				<iframe  style="width:100%;height:350px;" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.ca/maps?q=fairmont+hotel+winnipeg&amp;oe=utf-8&amp;client=firefox-a&amp;channel=sb&amp;ie=UTF8&amp;hq=fairmont+hotel&amp;hnear=Winnipeg,+Division+No.+11,+Manitoba&amp;t=m&amp;ll=49.895747,-97.137174&amp;spn=0.008816,0.006295&amp;output=embed&amp;zoom=5"></iframe>
+				</div>
+
+				<img src="images/fairmont_hotel.jpg" alt="fairmont hotel" style="width:100%;"/>
+					
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php 
+include "footer.php"; 
+?>
+</div>
+
+
+
+	<!-- JavaScript at the bottom for fast page loading -->
+
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+
+	<!--[if (lt IE 9) & (!IEMobile)]>
+	<script src="js/selectivizr-min.js"></script>
+	<![endif]-->
+
+
+	<!-- More Scripts-->
+	<script src="js/responsivegridsystem.js"></script>
+<script>
+      // Init responsive-nav.js
+      var nav = responsiveNav("#nav");
+    </script>
+
+</body>
+</html>
---
+++ Web.Admin/2014/news.php
@@ -0,0 +1,106 @@
+<?php 
+include "header.php"; 
+?> 
+
+<body>
+
+<div id="skiptomain"><a href="#maincontent">skip to main content</a></div>
+
+<div id="wrapper" >
+	<div id="maincontentcontainer" >
+		<div id="maincontent">
+			
+			<div class="section group">
+				<div class="col span_3_of_3" id="header">
+				<div id="mobileLogo"><img src="images/2014_logo.png" /></div>
+				<div id="logo"><img src="images/2014_logo.png" /></div>
+				<div id="socialLinks">Join Us:&nbsp;&nbsp;<a href="https://www.facebook.com/MBAofCanada" target="_blank"><img src="images/facebook_icon.png" /></a> <a href="https://twitter.com/MBANationals"><img src="images/twitter_icon.png" /></a> <a href="http://instagram.com/mb_master_bowlers"><img src="images/instagram_icon.png" /></a></div>
+				<div id="navigation">
+          <nav class="nav-collapse" >
+            <ul>
+              <li><a href="index.php" class="first">Home</a></li>
+              <li><a href="results.php">RESULTS</a></li>
+              <li><a href="news.php">NEWS</a></li>
+              <li><a href="schedule.php">SCHEDULE</a></li>
+              <li><a href="lanedraw.php">LANE DRAW</a></li>
+              <li><a href="photos.php">PHOTOS</a></li>
+              <li><a href="contingents.php">CONTINGENTS</a></li>
+			<li><a href="http://www.tnmpromostore.com/default.aspx?business_id=2495&page=main" target="_blank">SOUVENIRS</a></li>
+              <li><a href="centres.php">CENTRES</a></li>
+              <li><a href="hotel.php">HOTEL</a></li>
+            </ul>
+          </nav>
+        </div>				
+				</div>
+			</div>
+			<div class="section group">
+				<div class="col span_3_of_3" style="margin:0px;">
+				<div id="headerImage" style="background-image:url('images/header_image_7.jpg'); "></div>
+				<div id="photoCredit"><strong>The Forks Market</strong> &bull; Credit: Brent Bellamy</div>
+				</div>
+			</div>
+
+		<div class="section group content">
+				<div class="col span_1_of_3">
+				<div id="centres_links">
+				<a href="">APRIL</a><br />
+				<a href="">MAY</a><br />
+				<a href="">JUNE</a><br />
+				<a href="">JULY</a><br />
+				</div>
+				</div>
+				<div class="col span_2_of_3"  id="contentArea">
+					
+					<h2>NEWS</h2>
+				<h4>July 2, 2014<br />Singles Stepladder</span></h4><p>The frame by frame scores of the singles stepladder can be found here: <a href="http://s94662371.onlinehome.us/nationals2014/stepladder_men.php">MENS</a> & <a href="http://s94662371.onlinehome.us/nationals2014/stepladder_ladies.php">LADIES</a></p>
+
+				<h4>July 1, 2014<br />Unclaimed Social Prizes</span></h4><p>There are still unclaimed prizes from last night's social. They are yellow tickets: #3 - 2071, #10 - 2153, #11 - 2075, #13 - 2169, #16 - 2178, #17 - 2073, #18 - 2141, #21 - 2241, #24 - 2232. Grand prize #3 purple ticket - 2632 and the 50/50 205187. Please see a committee member if any of these numbers are yours.</p>
+			 	<h4>July 1, 2014<br />Happy Birthday Canada</span></h4><p>After a day at the lanes, we hope the weather cooperates, and everyone can head over to The Forks for some Canada Day fun! Tonight, Boston Pizza at 333 St. Mary Ave has team specials and prizes for the Master Bowlers, check your grab bags for the flyer and coupon to bring with you.</p>
+			 	<h4>July 1, 2014<br />Social</span></h4><p>We hope everyone enjoyed the Manitoba Night Social, and is ready to start day 3 of bowling!</p>
+			 	<h4>June 29, 2014<br />Update</span></h4><p>It was a WINDY, rainy day here in Winnipeg, which brought on a few problems. We appreciate everyone's patience while everything got resolved and rescheduled and are looking forward to tomorrow! On another note, a 410 was bowled by Stephen Reardon of NFLD Teaching Men at Academy Lanes and we are looking forward to seeing more high scores in the upcoming days. </p>
+			 	<h4>June 28, 2014<br />Bowling Starts Tomorrow!</span></h4><p>Everyone has arrived, opening ceremonies have taken place and tomorrow at 9am Central bowling begins! Good Luck!<br />Please sure to be on time for the buses!</p>
+
+					<h4>June 27, 2014<br />Getting Social</span></h4><p>We've joined Twitter and instagram, in addition to Facebook! Join us to follow along! <br />
+					<br /><a href="https://www.facebook.com/MBAofCanada" target="_blank"><img src="images/facebook_icon.png" /></a> <a href="https://twitter.com/MBANationals"><img src="images/twitter_icon.png" /></a> <a href="http://instagram.com/mb_master_bowlers"><img src="images/instagram_icon.png" /></a>
+					</p>
+					<h4>June 27, 2014<br />Welcome</span></h4><p>Welcome to all the early arrivals! Hope you are having a great time in Winnipeg!</p>
+
+					<h4>June 11, 2014<br />Lane Draw</span></h4><p>The lane draw is now available online, as well, the schedule has been updated with destination centres for each division.</p>
+					<h4>June 11, 2014<br />Souvenir Pre-Orders are now Closed</span></h4><p>Souvenirs will be available for purchase at the event on a first come first serve basis, as the opportunity to pre-order items on-line has ended.</p>
+					<h4>June 4, 2014<br /><span style="color:red;">Souvenir Pre-Order Deadline</span></h4><p>Be sure to visit our online store to pre-order your items for a discount! Cut-off is this <strong>Friday, June 6th!!!</strong> <br /><a href="http://www.tnmpromostore.com/default.aspx?business_id=2495&page=main" target="_blank">CLICK HERE</a></p>
+					<h4>May 9, 2014<br />Souvenirs</h4><p>Be sure to visit our online store to pre-order your items. <br /><a href="http://www.tnmpromostore.com/default.aspx?business_id=2495&page=main" target="_blank">CLICK HERE</a></p>
+					<h4>April 16, 2014<br />Web Site Launch</h4><p>Welcome to the 2014 MBAC Nationals Website</p>
+					
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php 
+include "footer.php"; 
+?>
+</div>
+
+
+
+	<!-- JavaScript at the bottom for fast page loading -->
+
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+
+	<!--[if (lt IE 9) & (!IEMobile)]>
+	<script src="js/selectivizr-min.js"></script>
+	<![endif]-->
+
+
+	<!-- More Scripts-->
+	<script src="js/responsivegridsystem.js"></script>
+<script>
+      // Init responsive-nav.js
+      var nav = responsiveNav("#nav");
+    </script>
+
+</body>
+</html>
