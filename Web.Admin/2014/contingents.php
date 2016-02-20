64071a06cd255582dfdfe716cf475368cf54c9c1
caffgeek <gibble@gmail.com>
2016-02-20 14:14:59 -0600

    link contingent teams and bowlers to their resutls pages

--- Web.Admin/2014/contingents.php
+++ Web.Admin/2014/contingents.php
@@ -60,7 +60,9 @@ include "header.php";
             <p>* Rookie</p>
 
             <div class="col span_5_of_12" data-ng-repeat="team in model.Teams | orderBy:'-1*Name.length'" data-ng-show="team.Id">
-              <h5>{{team.Name}}</h5>
+              <h5>
+                <a href="results.php#/team/{{team.Id}}">{{team.Name || 'Vacancy'}}</a>
+              </h5>
 
               <ol style="list-style-type:none;">
                 <li data-ng-if="team.Coach.Id">
@@ -79,7 +81,7 @@ include "header.php";
                 </li>
                 <li data-ng-repeat="bowler in team.Bowlers">
                   <span class="col span_9_of_12">
-                    {{bowler.Name || 'Vacancy'}}
+                    <a href="results.php#/bowler/{{bowler.Id}}">{{bowler.Name || 'Vacancy'}}</a>
                     <span data-ng-show="bowler.IsRookie">
                       <sup>*</sup>
                     </span>
