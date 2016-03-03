856dfc356b1fb1822094b576f1dad6f47686927b
ffishy <charlenemcivor@charlenes-mbp.gateway.mts.net>
2016-03-03 11:46:34 -0600

    Homepage Changes
    Add dynamic news to homepage, comment out sponsors till we have some,
    Adjust Newsletter Signup

--- Web.Admin/2016/index.php
+++ Web.Admin/2016/index.php
@@ -26,8 +26,14 @@ include 'header.php';
         	<div class="row">
         		<div class="col-md-6">
           <h2>News</h2>
-          <p>Stay Tuned for updates!</p>
-          <!--<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+          <div ng-repeat="newsItem in vm.News">
+              <h4>
+                <span class="newsHeader">{{newsItem.Title}}</span>
+              </h4>
+              <p style="white-space: pre-wrap;">{{newsItem.Content}}</p> 
+            </div>
+          </div>
+          <p><a class="btn btn-default" href="news.php" role="button">More News</a></p>
        			</div>       		
         		<div class="col-md-6">
           <!--<h2>Forms</h2>
@@ -40,17 +46,69 @@ include 'header.php';
        		</div>	
         </div>
         <div class="col-md-4">
-          <h2>Sponsors</h2>
-          <p>Coming soon</p>
+         <h2>SPONSORS</h2>
+                <div style="height:250px;" data-ng-controller="SponsorsController as vm">
+                  <div class="innerContainer">
+                    <a href="{{vm.CurrentSponsor.Website}}" target="_blank">
+                      <img ng-src="{{vm.ImageBase}}/Setup/Sponsors/Image/{{vm.CurrentSponsor.Id}}" style="width:100%;" alt="" border="0" />
+                    </a>
+                  </div>
+                </div>          
 		<h2>Follow Us</h2>
          <h3>Newsletter</h3>
-         <iframe width="200" height="122" src="https://my.sendinblue.com/users/subscribe/js_id/2dew5/id/1" frameborder="0" scrolling="auto" allowfullscreen style="display: block;text-align:left;"></iframe>	
+         <!-- SendinBlue Signup Form HTML Code -->
+
+<div id="sib_embed_signup" style="padding: 10px;">
+    <div class="wrapper" style="position:relative;margin-left: auto;margin-right: auto;">
+        <input type="hidden" id="sib_embed_signup_lang" value="en">
+        <input type="hidden" id="sib_embed_invalid_email_message" value="That email address is not valid. Please try again">
+	    <input type="hidden" name="primary_type" id="primary_type" value="email">
+        <div id="sib_loading_gif_area" style="position: absolute;z-index: 9999;display: none;">
+            <img src="http://img.mailinblue.com/new_images/loader_sblue.gif" style="display: block;margin-left: auto;margin-right: auto;position: relative;top: 40%;">
+        </div>
+        <form class="description" id="theform" name="theform" action="https://my.sendinblue.com/users/subscribeembed/js_id/2dew5/id/1" onsubmit="return false;">
+            <input type="hidden" name="js_id" id="js_id" value="2dew5"><input type="hidden" name="listid" id="listid" value="2"><input type="hidden" name="from_url" id="from_url" value="yes"><input type="hidden" name="hdn_email_txt" id="hdn_email_txt" value="">
+            <div class="container rounded">
+                
+               <input class="hidden" type="hidden" name="req_hid" id="req_hid" value="">
+               <div class="header">
+                    <h1 class="title editable" data-editfield="newsletter_name" ></h1>
+  		    <h3 id="company-name" ></h3>
+                </div>
+                    <div class="description editable" data-editfield="newsletter_description" ></div>
+                    <div class="view-messages" style=" margin:5px 0;"> </div>
+                        <!-- an email as primary -->
+            <div class="primary-group email-group forms-builder-group ui-sortable" style="">
+                            <div class="row mandatory-email">
+                                <input type="text" name="email" id="email" value="">
+                                <div style="clear:both;"></div>
+                                <div class="hidden-btns">
+                                    <a class="btn move" href="#"><i class="icon-move"></i></a><br>
+                        <!--<a class="btn btn-danger delete"  href="#"><i class="icon-white icon-trash"></i></a>-->
+                                </div>
+                            </div>
+                         </div>
+                        <!-- end of primary -->
+                         <div class="byline">
+                         <button class="button editable " type="submit" data-editfield="subscribe" >Subscribe</button></div>
+                         <div style="clear:both;"></div>
+                        </div>
+        </form>
+    </div>
+</div>
+
+<!-- End : SendinBlue Signup Form HTML Code -->	
+         
+         
+         
           <div class="fb-page" style="margin-bottom:15px;" data-href="https://www.facebook.com/MBAofCanada/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/MBAofCanada/"><a href="https://www.facebook.com/MBAofCanada/">Master Bowlers Association of Canada</a></blockquote></div></div>
 			
           <div><a class="twitter-timeline" href="https://twitter.com/MBANationals" data-widget-id="702222211382259713">Tweets by @MBANationals</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
 
-          <!--<h2>High Scores</h2>
-          <p>Coming Soon</p>-->
+          <div data-ng-controller="HighscoresController">
+					      <div class="section group" id="highScores" data-ng-include="" data-src="'/ClientApp/views/highscores.html'">
+                </div>
+              </div>
 
         </div>
       </div>
