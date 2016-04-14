340da314f652a5169b382c26bc524d27e3d5e7e0
caffgeek <gibble@gmail.com>
2016-04-13 19:37:51 -0500

    update contingent to cross out replaced bowler

--- Web.Admin/2016/header.php
+++ Web.Admin/2016/header.php
@@ -26,7 +26,8 @@
 
     <!-- Custom styles for this template -->
     <link href="bootstrap/css/customize.css" rel="stylesheet">
-
+	
+    <link href="/ClientApp/Styles/WebUI.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
