9ccb4ba1ad81c606929aee7e66600485c00372b7
caffgeek <gibble@gmail.com>
2016-04-19 00:05:41 -0500

    Change ParticipantScoreQueries to json model with blob storage

--- ReadModels/ParticipantScoreQueries.cs
+++ ReadModels/ParticipantScoreQueries.cs
@@ -1,18 +1,25 @@
-﻿using Edument.CQRS;
+﻿using AzureTableHelper;
+using Edument.CQRS;
 using Events.Participant;
 using Events.Scores;
+using Microsoft.WindowsAzure.Storage;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 
 namespace MBACNationals.ReadModels
 {
-    public class ParticipantScoreQueries : BaseReadModel<ParticipantScoreQueries>,
+    public class ParticipantScoreQueries : //BaseReadModel<ParticipantScoreQueries>,
+        IReadModel,
         IParticipantScoreQueries,
         ISubscribeTo<ParticipantCreated>,
         ISubscribeTo<ParticipantAverageChanged>,
         ISubscribeTo<ParticipantGameCompleted>
     {
+        public List<Participant> Participants { get; private set; }
+
         public class Participant
         {
             public Guid Id { get; internal set; }
@@ -27,11 +34,16 @@ namespace MBACNationals.ReadModels
             public List<Score> Scores { get; internal set; }
             public int HighScore { get; internal set; }
             public int HighPOA { get; internal set; }
+
+            public Participant()
+            {
+                Scores = new List<Score>();
+            }
         }
 
         public class Score
         {
-            public string MatchId { get; internal set; }
+            public Guid MatchId { get; internal set; }
             public int Number { get; internal set; }
             public int Scratch { get; internal set; }
             public int POA { get; internal set; }
@@ -45,146 +57,98 @@ namespace MBACNationals.ReadModels
             public bool IsPOA { get; internal set; }
         }
 
-        private class TSParticipant : Entity
+        public ParticipantScoreQueries()
         {
-            public Guid Id
-            {
-                get { return Guid.Parse(RowKey); }
-                internal set { RowKey = value.ToString(); PartitionKey = value.ToString(); }
-            }
-            public string Name { get; set; }
-            public string Province { get; set; }
-            public string Division { get; set; }
-            public int Average { get; set; }
-            public int NationalGames { get; set; }
-            public int NationalTotal { get; set; }
-            public int NationalAverage { get; set; }
-            public double NationalWins { get; set; }
-            public int HighScore { get; set; }
-            public int HighPOA { get; set; }
+            Reset();
         }
-
-        private class TSGame : Entity
-        {
-            public Guid Id
-            {
-                get { return Guid.Parse(RowKey); }
-                internal set { RowKey = value.ToString();  }
-            }
-            public Guid ParticipantId
-            {
-                get { return Guid.Parse(PartitionKey); }
-                internal set { PartitionKey = value.ToString(); }
-            }
-            public int Number { get; set; }
-            public int Scratch { get; set; }
-            public int POA { get; set; }
-            public string WinLossTie { get; set; }
-            public int Lane { get; set; }
-            public string Centre { get; set; }
-            public string OpponentName { get; set; }
-            public string OpponentProvince { get; set; }
-            public int OpponentScratch { get; set; }
-            public int OpponentPOA { get; set; }
-            public bool IsPOA { get; set; }
-        }
-
+        
         public ParticipantScoreQueries.Participant GetParticipant(Guid id)
         {
-            var scores = Storage.Query<TSGame>(x => x.ParticipantId == id)
-                .Select(x => new Score
-                {
-                    MatchId = x.Id.ToString(),
-                    Number = x.Number,
-                    Scratch = x.Scratch,
-                    POA = x.POA,
-                    WinLossTie = x.WinLossTie,
-                    Lane = x.Lane,
-                    Centre = x.Centre,
-                    OpponentProvince = x.OpponentProvince,
-                    OpponentName = x.OpponentName,
-                    OpponentScratch = x.OpponentScratch,
-                    OpponentPOA = x.OpponentPOA,
-                    IsPOA = x.IsPOA
-                }).ToList();
+            return Participants.SingleOrDefault(x => x.Id == id);
+        }
 
-            var tsp = Storage.Read<TSParticipant>(id, id);
-            return new Participant
-            {
-                Id = tsp.Id,
-                Name = tsp.Name,
-                Province = tsp.Province,
-                Division = tsp.Division,
-                Average = tsp.Average,
-                NationalGames = tsp.NationalGames,
-                NationalTotal = tsp.NationalTotal,
-                NationalAverage = tsp.NationalAverage,
-                NationalWins = (decimal)tsp.NationalWins,
-                Scores = scores,
-                HighScore = scores.Max(x => x.Scratch),
-                HighPOA = scores.Max(x => x.POA),
-            };
+        public void Reset()
+        {
+            Participants = new List<Participant>();
+        }
+
+        public void Save()
+        {
+            //TODO: Move to baseclass
+            var jsonModel = JsonConvert.SerializeObject(this);
+
+            var storageConnection = ConfigurationManager.ConnectionStrings["AzureTableStorage"].ConnectionString;
+            var storageAccount = CloudStorageAccount.Parse(storageConnection);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var azureBlobHelper = new AzureBlobHelper(blobClient);
+            var container = azureBlobHelper.GetContainerFor("ReadModels");
+
+            var modelBlob = container.GetBlockBlobReference(this.GetType().Name);
+            modelBlob.UploadText(jsonModel);
         }
 
         public void Handle(ParticipantCreated e)
         {
-            Storage.Create(e.Id, e.Id, new TSParticipant
+            Participants.Add(new Participant
             {
+                Id = e.Id,
                 Name = e.Name
             });
         }
 
         public void Handle(ParticipantAverageChanged e)
         {
-            Storage.Update<TSParticipant>(e.Id, e.Id, x => x.Average = e.Average);
+            var participant = Participants.Single(x => x.Id == e.Id);
+            participant.Average = e.Average;
         }
 
         public void Handle(ParticipantGameCompleted e)
         {
-            var game = Storage.Read<TSGame>(e.ParticipantId, e.Id);
+            var participant = Participants.Single(x => x.Id == e.ParticipantId);
+            var game = participant.Scores.SingleOrDefault(x => x.MatchId == e.Id);
             if (game == null)
             {
-                Storage.Create<TSGame>(e.ParticipantId, e.Id, new TSGame
-                   {
-                       Id = e.Id,
-                       ParticipantId = e.ParticipantId,
-                       Number = e.Number,
-                       Scratch = e.Score,
-                       POA = e.POA,
-                       WinLossTie = e.IsPOA 
-                        ? (e.POA > e.OpponentPOA ? "W" : e.POA < e.OpponentPOA ? "L" : "T")
-                        : (e.Score > e.OpponentScore ? "W" : e.Score < e.OpponentScore ? "L" : "T"),
-                       Lane = e.Lane,
-                       Centre = e.Centre,
-                       OpponentProvince = e.Opponent,
-                       OpponentName = e.OpponentName,
-                       OpponentScratch = e.OpponentScore,
-                       OpponentPOA = e.OpponentPOA,
-                       IsPOA = e.IsPOA
-                   });
-
-                Storage.Update<TSParticipant>(e.ParticipantId, e.ParticipantId, x =>
+                participant.Scores.Add(new Score
                 {
-                    x.Name = e.Name;
-                    x.Division = e.Division;
-                    x.Province = e.Contingent;
-                    x.NationalGames += 1;
-                    x.NationalTotal += e.Score;
-                    x.NationalAverage = x.NationalTotal / x.NationalGames;
-                    x.NationalWins += (double)(e.Points > 0M ? ( e.Points % 1M == 0M ? 1M : .5M) : 0M);
+                    MatchId = e.Id,
+                    Number = e.Number,
+                    Scratch = e.Score,
+                    POA = e.POA,
+                    WinLossTie = e.IsPOA 
+                     ? (e.POA > e.OpponentPOA ? "W" : e.POA < e.OpponentPOA ? "L" : "T")
+                     : (e.Score > e.OpponentScore ? "W" : e.Score < e.OpponentScore ? "L" : "T"),
+                    Lane = e.Lane,
+                    Centre = e.Centre,
+                    OpponentProvince = e.Opponent,
+                    OpponentName = e.OpponentName,
+                    OpponentScratch = e.OpponentScore,
+                    OpponentPOA = e.OpponentPOA,
+                    IsPOA = e.IsPOA
                 });
+
+                participant.Name = e.Name;
+                participant.Division = e.Division;
+                participant.Province = e.Contingent;
+                participant.NationalGames += 1;
+                participant.NationalTotal += e.Score;
+                participant.NationalAverage = participant.NationalTotal / participant.NationalGames;
+                participant.NationalWins += e.Points > 0M ? (e.Points % 1M == 0M ? 1M : .5M) : 0M;
+                
+                participant.HighScore = Math.Max(e.Score, participant.HighScore);
+                participant.HighPOA = Math.Max(e.POA, participant.HighPOA);
             }
             else
             {
-                Storage.Update<TSParticipant>(e.ParticipantId, e.ParticipantId, x =>
-                {
-                    x.Name = e.Name;
-                    x.NationalTotal = x.NationalTotal - game.Scratch + e.Score;
-                    x.NationalAverage = x.NationalTotal / x.NationalGames;
-                    x.NationalWins = x.NationalWins
-                        - (double)(game.WinLossTie == "W" ? 1M : game.WinLossTie == "T" ? .5M : 0M)
-                        + (double)(e.Points > 0 ? (e.Points % 1M == 0M ? 1M : .5M) : 0M);
-                });
+                participant.Name = e.Name;
+                participant.NationalTotal = participant.NationalTotal - game.Scratch + e.Score;
+                participant.NationalAverage = participant.NationalTotal / participant.NationalGames;
+                participant.NationalWins = participant.NationalWins
+                    - (game.WinLossTie == "W" ? 1M : game.WinLossTie == "T" ? .5M : 0M)
+                    + (e.Points > 0 ? (e.Points % 1M == 0M ? 1M : .5M) : 0M);
+
+                participant.HighScore = participant.Scores.Max(x => x.Scratch);
+                participant.HighPOA = participant.Scores.Max(x => x.POA);
             }
         }
     }
