5da28e3dfcc71904f09a5a446f5b58b4ef36a1b0
ffishy <charlenemcivor@charlenes-mbp.gateway.mts.net>
2016-04-21 11:40:09 -0500

    Adding Facebook Photo Generator / Instagram Feed to Photos Page

--- Web.Admin/2016/footer.php
+++ Web.Admin/2016/footer.php
@@ -26,7 +26,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script type="text/javascript" src="path/to/instafeed.min.js"></script>
 	<script type='text/javascript' src='https://my.sendinblue.com/public/theme/version3/js/subscribe-validate.js'></script>
 	<script type='text/javascript'>
 	    jQuery.noConflict(true);
