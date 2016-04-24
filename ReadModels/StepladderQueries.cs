ab3f6361667545b5f5980bc5bdb7903e6cd3b96d
caffgeek <gibble@gmail.com>
2016-04-23 23:38:53 -0500

    Stepladder queries updated to blob json storage

--- ReadModels/StepladderQueries.cs
+++ ReadModels/StepladderQueries.cs
@@ -7,52 +7,19 @@ using System.Linq;
 
 namespace MBACNationals.ReadModels
 {
-    public class StepladderQueries : BaseReadModel<StepladderQueries>,
+    public class StepladderQueries :
+        IReadModel,
         IStepladderQueries,
-        ISubscribeTo<TournamentCreated>,
         ISubscribeTo<StepladderMatchCreated>,
         ISubscribeTo<StepladderMatchUpdated>,
         ISubscribeTo<StepladderMatchDeleted>
     {
+        public List<Match> Matches { get; set; }
+
         public class Match
         {
-            public Guid Id { get; internal set; }
-            public Guid TournamentId { get; internal set; }
-            public string Year { get; internal set; }
-            public Guid HomeId { get; internal set; }
-            public string HomeName { get; internal set; }
-            public string HomeShots { get; internal set; }
-            public Guid AwayId { get; internal set; }
-            public string AwayName { get; internal set; }
-            public string AwayShots { get; internal set; }
-            public bool IsComplete { get; internal set; }
-            public string Gender { get; internal set; }
-            public DateTime Created { get; internal set; }
-            public DateTime Updated { get; internal set; }
-        }
-
-        private class TSTournament : Entity
-        {
-            public Guid Id
-            {
-                get { return Guid.Parse(RowKey); }
-                internal set { RowKey = value.ToString(); PartitionKey = value.ToString(); }
-            }
-            public string Year { get; set; }
-        }
-
-        private class TSMatch : Entity
-        {
-            public Guid TournamentId
-            {
-                get { return Guid.Parse(PartitionKey); }
-                internal set { PartitionKey = value.ToString(); }
-            }
-            public Guid MatchId
-            {
-                get { return Guid.Parse(RowKey); }
-                internal set { RowKey = value.ToString(); }
-            }
+            public Guid Id { get; set; }
+            public Guid TournamentId { get; set; }
             public string Year { get; set; }
             public Guid HomeId { get; set; }
             public string HomeName { get; set; }
@@ -66,43 +33,32 @@ namespace MBACNationals.ReadModels
             public DateTime Updated { get; set; }
         }
 
+        public StepladderQueries()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Matches = new List<Match>();
+        }
+
+        public void Save()
+        {
+            ReadModelPersister.Save(this);
+        }
 
         public List<Match> GetMatches(string year)
         {
-            var tournament = Storage.Query<TSTournament>(x => x.Year == year).FirstOrDefault();
-            if (tournament == null)
-                return Enumerable.Empty<Match>().ToList();
-
-            return Storage.Query<TSMatch>(x => x.TournamentId == tournament.Id)
-                .Select(x => new Match
-                {
-                    Id = x.MatchId,
-                    Year = x.Year,
-                    TournamentId = x.TournamentId,
-                    HomeId = x.HomeId,
-                    HomeName = x.HomeName,
-                    HomeShots = x.HomeShots,
-                    AwayId = x.AwayId,
-                    AwayName = x.AwayName,
-                    AwayShots = x.AwayShots,
-                    IsComplete = x.IsComplete,
-                    Gender = x.Gender,
-                    Created = x.Created,
-                }).ToList();
+            return Matches.Where(x => x.Year == year).ToList();
         }
-
-        public void Handle(TournamentCreated e)
-        {
-            Storage.Create(e.Id, e.Id, new TSTournament
-            {
-                Year = e.Year
-            });
-        }
-
+        
         public void Handle(StepladderMatchCreated e)
         {
-            Storage.Create(e.TournamentId, e.Id, new TSMatch
+            Matches.Add(new Match
             {
+                Id = e.Id,
+                TournamentId = e.TournamentId,
                 Year = e.Year,
                 HomeId = e.Home,
                 HomeName = e.HomeBowler,
@@ -117,17 +73,15 @@ namespace MBACNationals.ReadModels
 
         public void Handle(StepladderMatchUpdated e)
         {
-            Storage.Update<TSMatch>(e.TournamentId, e.Id, x =>
-            {
-                x.AwayShots = e.AwayShots;
-                x.HomeShots = e.HomeShots;
-                x.Updated = e.Updated;
-            });
+            var match = Matches.SingleOrDefault(x => x.Id == e.Id);
+            match.AwayShots = e.AwayShots;
+            match.HomeShots = e.HomeShots;
+            match.Updated = e.Updated;
         }
 
         public void Handle(StepladderMatchDeleted e)
         {
-            Storage.Delete<TSMatch>(e.TournamentId, e.Id);
+            Matches.RemoveAll(x => x.Id == e.Id);
         }
     }
 }
