9578ed668dcb85dc87275de8e3290e52bc4ed00c
caffgeek <gibble@gmail.com>
2016-06-09 23:35:52 -0500

    tweak match display

--- Web.Admin/2016/results.php
+++ Web.Admin/2016/results.php
@@ -20,29 +20,30 @@ include 'header.php';
     	
       <!-- Example row of columns -->
       <div class="row"  data-ng-app="app">
-        <div class="col-md-2"  data-ng-controller="ResultsController">
-        <ul class="sidebarNav">
-       	<!--<li><a href="">Coming Soon</a></li>-->
-            <li><a ui-sref="standings({division: 'Tournament Men Single'})">TOURNAMENT MEN SINGLE</a></li>
-            <li><a ui-sref="standings({division: 'Tournament Ladies Single'})">TOURNAMENT LADIES SINGLE</a></li>
-            <li><a ui-sref="standings({division: 'Tournament Men'})">TOURNAMENT MEN</a></li>
-            <li><a ui-sref="standings({division: 'Tournament Ladies'})">TOURNAMENT LADIES</a></li>
-            <li><a ui-sref="standings({division: 'Teaching Men Single'})">TEACHING MEN SINGLE</a></li>
-            <li><a ui-sref="standings({division: 'Teaching Ladies Single'})">TEACHING LADIES SINGLE</a></li>
-            <li><a ui-sref="standings({division: 'Teaching Men'})">TEACHING MEN</a></li>
-            <li><a ui-sref="standings({division: 'Teaching Ladies'})">TEACHING LADIES</a></li>
-            <li><a ui-sref="standings({division: 'Seniors'})">SENIORS</a></li>
-            <li><a ui-sref="standings({division: 'Seniors Single'})">SENIORS SINGLE</a></li>
-            <hr/>
-            <li><a href="#Standings" data-ng-click="viewStepladder()">STEPLADDER</a></li>
-    	</ul>
+        <div class="col-sm-2"  data-ng-controller="ResultsController">
+          <ul class="sidebarNav">
+       	  <!--<li><a href="">Coming Soon</a></li>-->
+              <li><a ui-sref="standings({division: 'Tournament Men Single'})">TOURNAMENT MEN SINGLE</a></li>
+              <li><a ui-sref="standings({division: 'Tournament Ladies Single'})">TOURNAMENT LADIES SINGLE</a></li>
+              <li><a ui-sref="standings({division: 'Tournament Men'})">TOURNAMENT MEN</a></li>
+              <li><a ui-sref="standings({division: 'Tournament Ladies'})">TOURNAMENT LADIES</a></li>
+              <li><a ui-sref="standings({division: 'Teaching Men Single'})">TEACHING MEN SINGLE</a></li>
+              <li><a ui-sref="standings({division: 'Teaching Ladies Single'})">TEACHING LADIES SINGLE</a></li>
+              <li><a ui-sref="standings({division: 'Teaching Men'})">TEACHING MEN</a></li>
+              <li><a ui-sref="standings({division: 'Teaching Ladies'})">TEACHING LADIES</a></li>
+              <li><a ui-sref="standings({division: 'Seniors'})">SENIORS</a></li>
+              <li><a ui-sref="standings({division: 'Seniors Single'})">SENIORS SINGLE</a></li>
+              <hr/>
+              <li><a href="#Standings" data-ng-click="viewStepladder()">STEPLADDER</a></li>
+    	    </ul>
         </div>
 
-        <div class="col-md-10">
-        <h2>RESULTS</h2>
-      		<div ui-view=""></div>
-      		<h5 class="text-center">Results are unofficial</h5>
-	    </div>
+        <div class="col-sm-10">
+          <h2>RESULTS</h2>
+      		  <div class="col-md-12" ui-view=""></div>
+            
+            <h5 class="col-md-10 text-center">Results are unofficial</h5>
+	      </div>
       </div>
 
 <?php
