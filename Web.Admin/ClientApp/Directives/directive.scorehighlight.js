628c1c62b08038d6412907ce8fc851bbdb2f578d
caffgeek <gibble@gmail.com>
2017-06-23 19:18:57 -0500

    adding background highlights for wins/losses/ties

---
+++ Web.Admin/ClientApp/Directives/directive.scorehighlight.js
@@ -0,0 +1,31 @@
+﻿app.directive('poahighlight', [function ($location) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs, model) {
+            var $el = angular.element(element);
+            var $a = $el.children('a');
+            
+            var href = $a.attr('href');
+            var poa = attrs.poahighlight;
+            
+            $el.toggleClass('negative', (poa < 0));
+        }
+    };
+}]);
+
+app.directive('winhighlight', [function ($location) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs, model) {
+            var $el = angular.element(element);
+            var $a = $el.children('a');
+            
+            var href = $a.attr('href');
+            var wlt = attrs.winhighlight;
+
+            $el.toggleClass('win', (wlt == "W"));
+            $el.toggleClass('loss', (wlt == "L"));
+            $el.toggleClass('tie', (wlt == "T"));
+        }
+    };
+}]);
